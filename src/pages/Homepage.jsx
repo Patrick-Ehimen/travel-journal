f1be2ac4b303e3589ca3cd356da3cb2543943e93
Patrick-Ehimen <osepatrick.ehimen77@gmail.com>
2024-05-05 13:03:12 +0100

    Update Homepage.jsx

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,35 +1,25 @@
-import { Link } from "react-router-dom"; // Import Link component for navigation
-import PageNav from "../components/PageNav"; // Import PageNav component for page navigation
-import styles from "./Homepage.module.css"; // Import CSS module for Homepage
+import { Link } from "react-router-dom";
+import PageNav from "../components/PageNav";
+import styles from "./Homepage.module.css";
 
 export default function Homepage() {
   return (
     <main className={styles.homepage}>
-      {" "}
-      {/* // Main container with homepage styling */}
-      {/* // Navigation bar component */}
       <PageNav />
+
       <section>
-        {" "}
-        {/* // Section for the main content */}
         <h1>
-          {" "}
-          {/* // Main heading You travel the world. */}
+          You travel the world.
           <br />
-          {/* // Line break for better formatting WorldWise keeps track of
-          your adventures. */}
+          WorldWise keeps track of your adventures.
         </h1>
         <h2>
-          {" "}
-          {/* // Subheading with additional information A world map that tracks your
-          footsteps into every city you can think of. Never forget your
-          wonderful experiences, and show your friends how you have wandered the
-          world. */}
+          A world map that tracks your footsteps into every city you can think
+          of. Never forget your wonderful experiences, and show your friends how
+          you have wandered the world.
         </h2>
         <Link to="/login" className="cta">
-          {" "}
-          {/* // Link to the login page with CTA class Start tracking now // Call to
-          action text */}
+          Start tracking now
         </Link>
       </section>
     </main>
